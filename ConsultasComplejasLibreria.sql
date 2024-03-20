eb59452cfc0d3ee5b3044ef21ccbed2d1165face
Javier Galán Cortés <javier.galan2032@gmail.com>
2024-03-20 10:16:12 +0100

    Consulta 23

--- ConsultasComplejasLibreria.sql
+++ ConsultasComplejasLibreria.sql
@@ -3,6 +3,9 @@ SELECT book_name, CONCAT(book_price,'€') AS precioMenor, aut_name FROM libro L
 WHERE A.aut_id=L.aut_id AND
 book_price=(SELECT MIN(book_price) AS precioMenor FROM libro);
 
+SELECT book_name, CONCAT(book_price,'€') AS precioMenor, aut_name FROM libro L NATURAL JOIN autor A
+WHERE book_price=(SELECT MIN(book_price) AS precioMenor FROM libro);
+
 -- 21
 SET @dolar=1.09;
 SELECT MAX(book_price) AS precioMayor FROM libro;
@@ -17,3 +20,7 @@ WHERE L.pub_id=P.pub_id
 GROUP BY pub_name;
 
 -- 23
+SELECT pub_name, COUNT(*) AS nPulicaciones
+FROM libro L NATURAL JOIN publicacion P
+WHERE country='USA'
+GROUP BY pub_name;
